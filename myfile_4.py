6740c5d735e14b87e0ff7c967c3a26937a6762c5
Manas Agrawal <manasagrawal143@gmail.com>
2025-05-21 11:37:30 +0530

    Create myfile_4.py

---
+++ myfile_4.py
@@ -0,0 +1,11 @@
+questions = [
+        "Corporate identity number (CIN) of company",
+        "Financial year to which financial statements relates",
+        "Product or service category code (ITC/ NPCS 4 digit code)",
+        "Description of the product or service category",
+        "Turnover of the product or service category (in Rupees)",
+        "Highest turnover contributing product or service code (ITC/ NPCS 8 digit code)",
+        "Description of the product or service",
+        "Turnover of highest contributing product or service (in Rupees)"
+    ]
+
